e92a0eef899fa3239a847070bc55cad91a9c8b82
hwestman97 <36103995+hwestman97@users.noreply.github.com>
2018-09-05 15:38:42 +0200

    Add files via upload
    float inte int

--- lab2.py
+++ lab2.py
@@ -7,9 +7,14 @@ Created on Tue Sep  4 17:30:16 2018
 from math import factorial, sqrt
 from time import sleep
 'rad i, kolumn j'
+
 '''Det här är lab2, med del A: pascals triangel och del B: medelvärde och standardavvikelse'''
 
+
 def user_choice():
+    '''
+    Press P for Pascals triangle and M for standard deviation and mean
+    '''
     x = True
     while x==True:
         user_input = input('Tryck P för Pascals triangel eller M för medelvärde och standardavvikelse: ')
@@ -20,6 +25,9 @@ def user_choice():
             print('Try again!')
 
 def rows():
+    '''
+    Enter a positive integer
+    '''
     y = True
     while y==True:
         user_num = input('Hur många rader av Pascals triangel vill du skriva ut? ')
@@ -34,6 +42,9 @@ def rows():
             print('Not a number, try again!')
             
 def pascals_triangel(n):
+    '''
+    takes user input number of rows and prints pascals triangel
+    '''
     for i in range(0, n):
         rad = ''
         for j in range(0, i+1):
@@ -43,26 +54,38 @@ def pascals_triangel(n):
         sleep(1)
 
 def binom(i, j):
+    '''
+    calculates the binominal factor for row i and column j
+    '''
     number = int(factorial(i)/(factorial(j) * (factorial(i-j))))
     return number
 
       
 def mean_standard(file):
+    '''
+    enter a file name with one number per line, the program returns mean and standard deviation
+    '''
     A = 0
     B = 0
     text = open(file, 'r')
     n = 0
     for line in text:
         n += 1
-        number = int(line)
+        number = float(line)
         A += number
         B += number**2
-    mu = A/n
-    sigma = sqrt((B + n*mu**2 - 2*mu*A) / (n - 1))
-    print (u'\u03BC' + '=' + str(mu))
-    print(u'\u03C3' + '=' + str(sigma))
+    if n > 0:
+        mu = A/n
+        sigma = sqrt((B + n*mu**2 - 2*mu*A) / (n - 1))
+        print (u'\u03BC' + '=' + str(mu))
+        print(u'\u03C3' + '=' + str(sigma))
+    else:
+        print('Din fil var tom :(')
 
 def start():
+    '''
+    the mainframe of the program
+    '''
     print('''Det här programet låter dig skriva ut n rader av pascals triangel \neller beräkna medelvärde och standardavvikelse av siffror i en textfil''')
     choice = user_choice()
     if choice == 'P':
@@ -70,6 +93,7 @@ def start():
         print('Calculating...')
         sleep(1)
         pascals_triangel(n)
+        print('Math is truly beautiful!')
     elif choice == 'M':
         file = input('Vad är namnet på din fil? ')
         mean_standard(file)
