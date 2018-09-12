6fab1e09bffd3b542d032d05d871ebc9819d408f
Hanna <ha.westman@gmail.com>
2018-09-12 13:25:38 +0100

    Tagit bort lite onödig kod, allt funkar

--- pascal+stdmean.py
+++ pascal+stdmean.py
@@ -1,63 +1,64 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep  4 17:30:16 2018
-
 @author: Hanna
 """
 from math import factorial, sqrt
 from time import sleep
-'rad i, kolumn j'
 
 '''Det här är lab2, med del A: pascals triangel och del B: medelvärde och standardavvikelse'''
 
 
 def user_choice():
     '''
-    Press P for Pascals triangle and M for standard deviation and mean
+    Frågar användaren om datorn ska köra Pascals triangel eller beräkna standardavvikelse/medelvärde
     '''
     x = True
     while x==True:
         user_input = input('Tryck P för Pascals triangel eller M för medelvärde och standardavvikelse: ')
         if user_input == 'P' or user_input == 'M':
-            x=False
+            x = False
             return user_input
         else:
-            print('Try again!')
+            print('Försök igen!')
 
 def rows():
     '''
-    Enter a positive integer
+    Hämtar indata från användaren och testar om det faktiskt är ett positivt heltal
+    Använder sig av try/except för att testa om indatan är ett heltal
     '''
     y = True
     while y==True:
         user_num = input('Hur många rader av Pascals triangel vill du skriva ut? ')
         try:
             number = int(user_num)
-            if number > 0:
+            if number > 0 and number < 20:
                 y = False
                 return number
+            elif number <= 0:
+                print('Inte ett positivt heltal, försök igen!')
             else:
-                print('Not a positivte number, try again!')
+                print('Orkar inte, gör det själv')
         except:
-            print('Not a number, try again!')
+            print('Inte ett heltal, försök igen!')
             
 def pascals_triangel(n):
     '''
-    takes user input number of rows and prints pascals triangel in a dramatic, slow way.
+    Tar indata antal rader av Pascals triangel och skriver ut dem på ett långsamt och dramatiskt vis.
     '''
     for i in range(0, n):
         rad = ''
         for j in range(0, i+1):
             number = str(binom(i,j))
-            rad += number + ' '
+            rad += number  + ' '
         print (rad)
         sleep(1)
     if n > 2:
-        print('Math is truly beautiful!')
+        print('Matematik är verkligen vackert!')
 
 def binom(i, j):
     '''
-    calculates the binominal factor for row i and column j
+    beräknar binominalfaktorn för rad i och kolumn j
     '''
     number = int(factorial(i)/(factorial(j) * (factorial(i-j))))
     return number
@@ -65,11 +66,12 @@ def binom(i, j):
       
 def mean_standard(file):
     '''
-    enter a file name with one number per line, the program returns mean and standard deviation
+    skriv in filnamnet, funktionen beräknar standardavvikelse och medelvärde
     '''
     A = 0
     B = 0
     n = 0
+    #with open() as f:
     text = open(file, 'r')
     for line in text:
         n += 1
@@ -79,23 +81,24 @@ def mean_standard(file):
     if n > 0:
         mu = A/n
         sigma = sqrt((B + n*mu**2 - 2*mu*A) / (n - 1))
-        print (u'\u03BC' + '=' + str(mu))
-        print(u'\u03C3' + '=' + str(sigma))
+        print('Medelvärdet är ' + u'\u03BC' + ' = ' + str(mu))
+        print('Standardavvikelsen är ' + u'\u03C3' + ' = ' + str(sigma))
     else:
         print('Din fil var tom :(')
 
 def main():
     '''
-    the mainframe of the program
+    programmets mainframe
     '''
     print('''Det här programet låter dig skriva ut n rader av pascals triangel \neller beräkna medelvärde och standardavvikelse av siffror i en textfil''')
     choice = user_choice()
     if choice == 'P':
         n = rows()
-        print('Calculating...')
+        print('Beräknar...')
         sleep(1)
         pascals_triangel(n)
     elif choice == 'M':
         file = input('Vad är namnet på din fil? ')
         mean_standard(file)
+
 main()
